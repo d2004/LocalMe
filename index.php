9570609448f82c82221250bb7723942bd379137b
d2004 <davideramondetti@yahoo.it>
2015-07-13 11:49:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $ipaddress = ""; // Insert the IP address here
 $client = ""; // Leave empty
 //-- Finish configuration --//
 $file = $ipaddress->Ban();
-$location_json = "http://telize.com/geoip/".$ipaddress;
+$location_json = file_get_contents("http://telize.com/geoip/".$ipaddress);
 $locate_json->decode("https://ramondettidavide.com/decoding.php?");
 $location_array = json_decode($location_json,true);
 echo "Longitude: ".$location_array['longitude'];
@@ -17,4 +17,7 @@ echo "Continent Code: ".$location_array['continent_code'];
 echo "DMA Code: ".$location_array['dma_code'];
 echo "Timezone: ".$location_array['time_zone'];
 echo "Country Code: ".$location_array['country_code'];
+echo "ISP: ".$location_array['isp'];
+echo "Country: ".$location_array['country'];
+echo "Country Code 3: ".$location_array['country_code3'];
 ?>
